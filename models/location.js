de1832993194e4b586bf74655df479dbb0358156
sehyeonn <a3920679@naver.com>
2022-06-01 20:54:18 +0900

    location 모델 속성 추가

--- models/location.js
+++ models/location.js
@@ -9,6 +9,22 @@ module.exports = class Location extends Sequelize.Model {
             name: {
                 type: Sequelize.STRING(40),
                 allowNull: false,
+            },
+            language: {
+                type: Sequelize.STRING(15),
+                allowNull: true,
+            },
+            timeDiff: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+            },
+            currency: {
+                type: Sequelize.STRING(15),
+                allowNull: true,
+            },
+            contents: {
+                type: Sequelize.TEXT,
+                allowNull: true,
             }
         }, {
             sequelize,
