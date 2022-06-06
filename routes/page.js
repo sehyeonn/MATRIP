27cc31686f80a59dc7b907b001ebd9678a34d519
sehyeonn <a3920679@naver.com>
2022-06-07 02:02:37 +0900

    tripDetail.html 에서 넌적스 날짜 비교 오류 수정, 여행지 상세 페이지에서 해당 여행지의 내 여행과 다른 사람의 여행을 볼 수 있게 하는 기능 구현

--- routes/page.js
+++ routes/page.js
@@ -3,8 +3,8 @@
 //***************************
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const { Location, Trip, TripDetail, Spot, sequelize } = require('../models');
-const { first } = require('nunjucks/src/filters');
+const { Location, Trip, TripDetail, Spot, sequelize, User } = require('../models');
+const { Op } = require('sequelize');
 
 const router = express.Router();
 
@@ -15,16 +15,21 @@ router.use((req, res, next) => {
 
 // 로그인 되어있으면 main.html 렌더링, 안되어있으면 /login으로 redirect 되어 로그인 하게 함
 router.get('/', isLoggedIn, async (req, res, next) => {
-    const trips = await Trip.findAll({
-        where: { UserId: req.user.id },
-        include: {
-            model: Location,
-            attributes: ['name'],
-        }
-    });
-    res.render('main', {
-        trips,
-    });
+    try {
+        const trips = await Trip.findAll({
+            where: { UserId: req.user.id },
+            include: {
+                model: Location,
+                attributes: ['name'],
+            }
+        });
+        res.render('main', {
+            trips,
+        });
+    } catch(err) {
+        console.error(err);
+        next(err);
+    }
 });
 
 // 로그인 화면 렌더링
@@ -77,51 +82,98 @@ router.get('/location/detail/:LocationId', isLoggedIn, async (req, res, next) =>
 
 // 해당 여행지의 관광지 리스트를 전달해주는 라우터
 router.get('/spots/:locationId', isLoggedIn, async (req, res, next) => {
-    const location = await Location.findOne({
-        where: { id: req.params.locationId },
-        include: [{
-            model: Spot,
-            attributes: ['id', 'name', 'type', 'contents'],
-        }],
-    });
-    res.send(location.Spots);
+    try {
+        const location = await Location.findOne({
+            where: { id: req.params.locationId },
+            include: [{
+                model: Spot,
+                attributes: ['id', 'name', 'type', 'contents'],
+            }],
+        });
+        res.send(location.Spots);
+    } catch(err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+// 해당 여행지의 내 여행 리스트를 전달해주는 라우터
+router.get('/trips/my/:locationId', isLoggedIn, async (req, res, next) => {
+    try {
+        const trips = await Trip.findAll({
+            where: { 
+                LocationId: req.params.locationId,
+                UserId: req.user.id,
+            },
+        });
+        res.send(trips);
+    } catch(err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+// 해당 여행지의 다른 사용자들의 여행 리스트를 전달해주는 라우터
+router.get('/trips/:locationId', isLoggedIn, async (req, res, next) => {
+    try {
+        const trips = await Trip.findAll({
+            where: { 
+                LocationId: req.params.locationId,
+                UserId: { [Op.not]: req.user.id },
+            },
+            include: [{
+                model: User,
+                attributes: ['username'],
+            }],
+        });
+        res.send(trips);
+    } catch(err) {
+        console.error(err);
+        next(err);
+    }
 });
 
 // 선택한 여행 상세 정보 화면 렌더링
 router.get('/tripDetail/:tripId', isLoggedIn, async (req, res, next) => {
-    const trip = await Trip.findOne({
-        where: { id: req.params.tripId },
-        include: {
-            model: Location,
-            attributes: ['name'],
+    try {
+        const trip = await Trip.findOne({
+            where: { id: req.params.tripId },
+            include: {
+                model: Location,
+                attributes: ['name'],
+            }
+        });
+        const tripDetails = await TripDetail.findAll({
+            where: { TripId: req.params.tripId },
+            include: [{
+                model: Spot,
+                attributes: ['name', 'lat', 'lng', 'type'],
+            }],
+            order: [
+                ['date'], ['number']
+            ]
+        });
+
+        const dates = [];
+        let date = new Date(trip.start);
+        while(date <= new Date(trip.end)) {
+            dates.push(`${date.getUTCFullYear()}-${String(date.getMonth()+1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`);
+            date.setDate(date.getDate() + 1);
         }
-    });
-    const tripDetails = await TripDetail.findAll({
-        where: { TripId: req.params.tripId },
-        include: [{
-            model: Spot,
-            attributes: ['name', 'lat', 'lng', 'type'],
-        }],
-        order: [
-            ['date'], ['number']
-        ]
-    });
 
-    const dates = [];
-    let date = new Date(trip.start);
-    while(date <= new Date(trip.end)) {
-        dates.push(`${date.getUTCFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
-        date.setDate(date.getDate() + 1);
+        res.render('tripDetail', {
+            trip,
+            tripDetails,
+            dates,
+            mapApiKey: process.env.MAP_API_KEY,
+        });
+    } catch(err) {
+        console.error(err);
+        next(err);
     }
-
-    res.render('tripDetail', {
-        trip,
-        tripDetails,
-        dates,
-        mapApiKey: process.env.MAP_API_KEY,
-    });
 });
 
+// 내 여행 일정에서 장소 추가 클릭하면 나오는 장소 선택 페이지를 렌더링
 router.get('/spot/select/:tripId/:locationId/:date', isLoggedIn, (req, res, next) => {
     res.render('selectSpot', {
         locationId: req.params.locationId,
